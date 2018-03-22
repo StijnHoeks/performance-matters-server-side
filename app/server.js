44152495fe5c3700ed8051e9ded8953b6cd4c7a3
Unknown <slqhoeks@gmail.com>
2018-03-22 15:30:50 +0100

    update

--- app/server.js
+++ app/server.js
@@ -1,23 +1,23 @@
 var express = require('express')
 var request = require('request')
 var app = express()
-var host = 'https://stijnhoeks.github.io/performance-matters-server-side/app/'
+var host = 'https://stijnhoeks.github.io/performance-matters-server-side'
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/movies', function (req, res) {
+app.get('/app', function (req, res) {
   request(host, function (error, response, body) {
     var data = JSON.parse(body)
-    res.render('index.ejs', {movies: data})
+    res.render('index.ejs', {data: data})
   });
 })
 
-app.get('/movies/:id', function (req, res) {
+app.get('/app/:id', function (req, res) {
   request(host + req.params.id, function (error, response, body) {
     var data = JSON.parse(body)
-    res.render('detail.ejs', {movie: data})
+    res.render('detail.ejs', {data: data})
   });
 })
 
---
+++ simple-server/server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var request = require('request')
+var app = express()
+var host = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa';
+/*var host = 'http://dennistel.nl/movies/'*/
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', function (req, res) {
+  request(host, function (error, response, body) {
+    var data = JSON.parse(body)
+    res.render('index', {data: data})
+  });
+})
+
+app.get('/:id', function (req, res) {
+  request(host/* + req.params.photos*/, function (error, response, body) {
+    var data = JSON.parse(body);
+    /*var photos = req.params;*/
+    console.log(data);
+    res.render('detail', {
+    	data: data/*,
+      photos: photos*/
+    })
+  });
+})
+
+var server = app.listen(1337, function () {
+   console.log('server is running on port 1337')
+})
