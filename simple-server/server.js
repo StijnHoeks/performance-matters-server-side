225df4975fe6fea5410661f9e124156e8fba5574
Unknown <slqhoeks@gmail.com>
2018-03-23 12:35:42 +0100

    update

--- simple-server/server.js
+++ simple-server/server.js
@@ -29,7 +29,7 @@ app.get('/:id', function (req, res) {
   request(host + req.params.photos, function (error, response, body) {
     var data = req.session.data;
     var photos = req.params;
-    var id = req.params.id;
+    /*var id = req.params.id;*/
     console.log(req.session.data);
     res.render('detail', {
     	data: data,
