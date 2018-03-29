3ad1e203000cf09fcea1801c6b11710ece9e93bf
Unknown <slqhoeks@gmail.com>
2018-03-29 12:20:35 +0200

    update

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,9 +1,11 @@
+
 var express = require('express')
 var request = require('request')
 var app = express()
 var host = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa';
 
 app.use(express.static('public'))
+app.use('/js', express.static(__dirname + '/js'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -24,3 +26,6 @@ app.get('/:id', function (req, res) {
 var server = app.listen(1337, function () {
    console.log('server is running on port 1337')
 })
+	
+
+
